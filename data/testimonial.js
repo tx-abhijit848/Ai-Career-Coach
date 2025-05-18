79b6665b004a99fd148a27fdbdf1b6cb8b64cec7
tx-abhijit848 <nandiabhijit60@gmail.com>
2025-05-18 23:35:48 +0530

    Update testimonial.js

--- data/testimonial.js
+++ data/testimonial.js
@@ -3,7 +3,7 @@ export const testimonial = [
       quote:
         "The AI-powered interview prep was a game-changer.It helps me to prepare a dream job!",
       author: "Sakshi Sharma",
-      image: "https://randomuser.me/api/portraits/women/57.jpg",
+      //image: "https://randomuser.me/api/portraits/women/57.jpg",
       role: "BTech in Computer Science",
       company: "IIT Kharagpur",
     },
@@ -11,7 +11,7 @@ export const testimonial = [
       quote:
         "The industry insights helped me pivot my career, i really appreciate it!!",
       author: "Kanishk Karn",
-      image: "https://randomuser.me/api/portraits/men/75.jpg",
+      //image: "https://randomuser.me/api/portraits/men/75.jpg",
       role: "BTech in Electronics and Communication",
       company: "NIT Jalandhar",
     },
@@ -19,7 +19,7 @@ export const testimonial = [
       quote:
         "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
       author: "Riya Gupta",
-      image: "https://randomuser.me/api/portraits/women/74.jpg",
+      //image: "https://randomuser.me/api/portraits/women/74.jpg",
       role: "BTech in Mechanical",
       company: "NIT Durgapur",
     },
